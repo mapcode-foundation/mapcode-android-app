cc523eaef347f30e442b6dce2b50c7a88ed23187
sds100 <developer.sds100@gmail.com>
2022-05-31 21:39:51 +0200

    #1 feat: show map
    Signed-off-by: sds100 <developer.sds100@gmail.com>

--- app/src/main/java/com/mapcode/ui/map/MapScreen.kt
+++ app/src/main/java/com/mapcode/ui/map/MapScreen.kt
@@ -1,22 +1,32 @@
 package com.mapcode.ui.map
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.maps.android.compose.GoogleMap
 
 /**
  * Created by sds100 on 31/05/2022.
  */
 
 @Composable
+fun Map(modifier: Modifier = Modifier) {
+    GoogleMap(
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+@Composable
 fun MapScreen(viewModel: MapViewModel) {
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-        Text("Test")
+        Column {
+            Map(Modifier.weight(0.7f))
+        }
     }
 }
 
