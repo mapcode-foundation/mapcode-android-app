14e2e0ff0d195ada223923031fe8da1e5704326f
sds100 <developer.sds100@gmail.com>
2022-06-01 09:38:20 +0200

    #6 feat: show my location button on map

--- app/src/main/java/com/mapcode/ui/map/MapScreen.kt
+++ app/src/main/java/com/mapcode/ui/map/MapScreen.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
+import com.mapcode.R
 
 /**
  * Created by sds100 on 31/05/2022.
@@ -17,7 +21,20 @@ import com.google.maps.android.compose.GoogleMap
 
 @Composable
 fun Map(modifier: Modifier = Modifier) {
-    GoogleMap(modifier = modifier.fillMaxSize())
+    var uiSettings by remember { mutableStateOf(MapUiSettings(myLocationButtonEnabled = true)) }
+    val properties by remember { mutableStateOf(MapProperties(isMyLocationEnabled = true)) }
+
+    uiSettings = uiSettings.copy(myLocationButtonEnabled = true)
+    GoogleMap(
+        modifier = modifier.fillMaxSize(),
+        uiSettings = uiSettings,
+        properties = properties,
+        onMyLocationButtonClick = {
+            false
+        },
+        onMyLocationClick = {},
+        contentDescription = stringResource(R.string.google_maps_content_description),
+    )
 }
 
 @Composable
